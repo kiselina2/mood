0e6891eda1f5d974e00df3de22ff1fabd92cb0ae
kiselina <62677656+kiselina2@users.noreply.github.com>
2026-04-03 17:52:43 +0200

    we got ourselves a memory leak! hell yea

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,13 @@ mod utils;
 
 use std::{pin::pin, time::Duration};
 
+use anyhow::anyhow;
 use dotenvy::dotenv;
 use rustls::crypto::aws_lc_rs::default_provider;
+use scap::{
+    capturer::Capturer,
+    frame::{BGRxFrame, Frame},
+};
 use tokio::{select, time::interval};
 
 use crate::{
@@ -19,6 +24,10 @@ use crate::{
 async fn main() -> anyhow::Result<()> {
     dotenv().ok();
 
+    default_provider()
+        .install_default()
+        .expect("couldn't install aws_lc_rs default crypto provider");
+
     let mut settings = AppSettings::load()?;
 
     if std::env::args().any(|a| a == "--setup") {
@@ -26,28 +35,72 @@ async fn main() -> anyhow::Result<()> {
         return Ok(());
     }
 
-    default_provider()
-        .install_default()
-        .expect("couldn't install aws_lc_rs default crypto provider");
+    // Check if the platform is supported
+    if !scap::is_supported() {
+        return Err(anyhow!("❌ Platform not supported"));
+    }
 
-    let (r, g, b) = (100u16 * 255, 255u16 * 255, 10u16 * 255);
+    // Check if we have permission to capture screen
+    // If we don't, request it.
+    if !scap::has_permission() && !scap::request_permission() {
+        return Err(anyhow!("❌ Permission denied"));
+    }
+
+    let options = scap::capturer::Options {
+        fps: 1,
+        output_resolution: scap::capturer::Resolution::_480p,
+        ..Default::default()
+    };
+
+    let mut capturer = Capturer::build(options)?;
 
     let mut shutdown = pin!(graceful_shutdown_signal());
     let mut ticker = interval(Duration::from_millis(20));
     ticker.set_missed_tick_behavior(tokio::time::MissedTickBehavior::Skip);
 
     let mut hue_entertainment = Hue::new(settings)?.start_entertainment().await?;
+    capturer.start_capture();
+
+    let mut loop_body = async || {
+        let frame = match capturer.get_next_frame() {
+            Err(e) => {
+                dbg_print!("{e}");
+                return;
+            }
+            Ok(frame) => frame,
+        };
+
+        let Frame::BGRx(frame) = frame else {
+            return;
+        };
+
+        let middle_pixel_index: usize = (4 * frame.width * (frame.height / 2)) as usize;
+
+        let [r, g, b]: &[u8; 3] = &frame.data[middle_pixel_index..middle_pixel_index + 3]
+            .try_into()
+            .unwrap();
+
+        let r: u16 = *r as u16 * 255;
+        let g: u16 = *g as u16 * 255;
+        let b: u16 = *b as u16 * 255;
+
+        println!("{r} {g} {b}"); // IF left uncommented, leaks memory
+
+        let colors = &[Color::new(r, g, b), Color::new(r, g, b)];
+
+        if let Err(e) = hue_entertainment.send_colors(colors).await {
+            dbg_print!("{e}");
+        }
+    };
 
     loop {
         select! {
             _ = &mut shutdown => { break }
-            _ = ticker.tick() => {
-                if let Err(e) = hue_entertainment.send_colors(&[Color::new(r, g, b), Color::new(r, g, b)]).await {
-                    dbg_print!("{e}");
-                }
-            }
+            _ = ticker.tick() => loop_body().await
         }
     }
+
+    capturer.stop_capture();
 
     Ok(())
 }
